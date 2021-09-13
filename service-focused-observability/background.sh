78482de901111a65e09eb54c4fd949c2ab8aebc3
tammergalal <tammer.galal@datadoghq.com>
2021-09-13 16:54:49 -0400

    working on some updates

--- service-focused-observability/background.sh
+++ service-focused-observability/background.sh
@@ -2,12 +2,17 @@
 
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
-mkdir /ecommerce-observability
-git clone https://github.com/DataDog/ecommerce-workshop /ecommerce-observability
-cd /ecommerce-observability
+touch /root/status.txt
+sleep 1
+STATUS=$(cat /root/status.txt)
+echo ""> /root/status.txt
+
+mkdir /ecommworkshop
+git clone https://github.com/DataDog/ecommerce-workshop /ecommworkshop
+cd ../ecommworkshop
 
 # locked to specific commit on 2020-10-02
 git checkout 9ce34516d9a65d6f09a6fffd5c4911a409d31e3f
 git reset --hard
 
-
+echo "complete">>/root/status.txt
