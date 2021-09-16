4c24f4c8367486d38e1ef02ed6bd01a9d8eb5025
Shawn Potts <shawn.potts@datadoghq.com>
2021-09-16 10:01:43 -0700

    removing status check

--- service-focused-observability/background.sh
+++ service-focused-observability/background.sh
@@ -2,21 +2,21 @@
 
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
-touch /root/status.txt
-sleep 1
-STATUS=$(cat /root/status.txt)
+# touch /root/status.txt
+# sleep 1
+# STATUS=$(cat /root/status.txt)
 
-if [ "$STATUS" != "complete" ]; then
-  echo ""> /root/status.txt
+# if [ "$STATUS" != "complete" ]; then
+#   echo ""> /root/status.txt
   mkdir /ecommworkshop
   git clone https://github.com/DataDog/ecommerce-workshop /ecommworkshop
   cd ../ecommworkshop
 
   # locked to specific commit on 2020-10-02
-  #git checkout 9ce34516d9a65d6f09a6fffd5c4911a409d31e3f
+  git checkout 9ce34516d9a65d6f09a6fffd5c4911a409d31e3f
   git reset --hard
 
-  echo "complete">>/root/status.txt
-if
+#   echo "complete">>/root/status.txt
+# if
 
-echo "Ready!"
+# echo "Ready!"
