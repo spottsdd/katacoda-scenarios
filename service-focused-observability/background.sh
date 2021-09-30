4ea6d0e8090440e29d24054bf5dbc7dbdcb83563
tammergalal <tammer.galal@datadoghq.com>
2021-09-30 09:48:55 -0400

    making some background changes for deprecated command

--- service-focused-observability/background.sh
+++ service-focused-observability/background.sh
@@ -5,6 +5,9 @@ curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 mkdir /ecommworkshop
 git clone https://github.com/DataDog/ecommerce-workshop /ecommworkshop
 cd ../ecommworkshop
+sudo sed -i "s/'analytics_enabled': true, //" ./store-frontend-broken-instrumented/config/initializers/datadog.rb
+sudo sed -i '80i \ \ \ \ command: ddtrace-run flask run --port=5002 --host=0.0.0.0' ./deploy/docker-compose/docker-compose-broken-instrumented.yml
+sudo sed -i '77i \ \ \ \ \ \ - DD_TRACE_SAMPLE_RATE=1.0' ./deploy/docker-compose/docker-compose-broken-instrumented.yml
 
 # # locked to specific commit on 2020-10-02
 # git checkout 9ce34516d9a65d6f09a6fffd5c4911a409d31e3f
